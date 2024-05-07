562ffbe8a84e7c0761234d396900c387ccb8f54f
Cameron Bateman <cameron.bateman@oracle.com>
2024-05-07 03:57:01 -0700

    Add required health check endpoint.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Et tu, Brut%sey?", name);
+	}
+
+	@GetMapping("/healthcheck")
+	public String healthCheck() {
+		return String.format("IAMHERE");
 	}
 }
